bb12f2d38d6b07e01a429e0dd605b63f64fcc560
StijnSmit <stijnsmit.nl@gmail.com>
2024-12-05 12:32:33 +0100

    JPEG model with all parsing corerct

--- src/main.rs
+++ src/main.rs
@@ -1,107 +1,149 @@
-use std::collections::HashMap;
 use std::fs;
+use std::fmt;
 
-mod entropy_coding;
-mod huffman_tree;
+//mod entropy_coding;
+//mod huffman_tree;
 
-#[repr(u16)]
-enum Headers {
-    StartOfImage = 0xffd8,
-    Application = 0xffe0,
-    QuantizationTable = 0xffdb,
-    StartOfFrame = 0xffc0,
-    HuffmanTable = 0xffc4,
-    StartOfScan = 0xffda, 
-    EndOfImage = 0xffd9,
+fn main() {
+    let img_data: Vec<u8> = fs::read("tutorial-image.jpg").unwrap();
+    let jpeg = JPEG::new(img_data);
+    println!("{:?}", jpeg);
 }
 
-struct jpeg {
+#[derive(PartialEq, Debug)]
+enum Markers {
+    StartOfImage,
+    ApplicationDefaultHeader,
+    QuantizationTable,
+    StartOfFrame,
+    DefineHuffmanTable,
+    StartOfScan,
+    EndOfImage,
+}
+
+impl Markers {
+    fn from_u16(marker: u16) -> Self {
+        match marker {
+            0xffd8 => Markers::StartOfImage,
+            0xffe0 => Markers::ApplicationDefaultHeader,
+            0xffdb => Markers::QuantizationTable,
+            0xffc0 => Markers::StartOfFrame,
+            0xffc4 => Markers::DefineHuffmanTable,
+            0xffda => Markers::StartOfScan,
+            0xffd9 => Markers::EndOfImage,
+            _ => {
+                panic!("There can't be an unknown header");
+            }
+        }
+    }
+
+    fn description(&self) -> &'static str {
+        match self {
+            Markers::StartOfImage => "Start of Image",
+            Markers::ApplicationDefaultHeader => "Application Default Header",
+            Markers::QuantizationTable => "Quantization Table",
+            Markers::StartOfFrame => "Start of Frame",
+            Markers::DefineHuffmanTable => "Define Huffman Table",
+            Markers::StartOfScan => "Start of Scan",
+            Markers::EndOfImage => "End of Image",
+        }
+    }
+}
+
+struct JPEG {
     qtables: Vec<QuantizationTable>,
     sof: ImageData,
     htables: Vec<HuffmanHeader>,
     raw_data: Vec<u8>,
 }
 
-impl jpeg {
-    fn new() -> Self {
-        unimplemented!()
-    }
+impl fmt::Debug for JPEG {
+   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Point")
+            .field("Image details: ", &self.sof)
+            .field("Quantized tables: ", &self.qtables.len())
+            .field("Huffman tables: ", &self.htables.len())
+            .field("Raw data of len: ", &self.raw_data.len())
+            .finish()
+    } 
 }
 
+impl JPEG {
+    fn new(img_data: Vec<u8>) -> Self {
+        let mut data = img_data.as_slice();
+        let mut qtables: Vec<QuantizationTable> = vec![];
+        let mut sof: ImageData = ImageData::empty();
+        let mut htables: Vec<HuffmanHeader> = vec![];
+        let mut raw_data: Vec<u8> = vec![];
 
-fn main() {
-    let marker_mapping = HashMap::from([
-        (0xffd8,  "Start of Image"),
-        (0xffe0, "Application Default Header"),
-        (0xffdb, "Quantization Table"),
-        (0xffc0, "Start of Frame"),
-        (0xffc4, "Define Huffman Table"),
-        (0xffda, "Start of Scan"),
-        (0xffd9, "End of Image"),
-    ]);
-
-    let img_data: Vec<u8> = fs::read("tutorial-image.jpg").unwrap();
-
-    let mut data = img_data.as_slice();
-
-    loop {
-        if data.len() < 2 {
-            return;
-        }
-        let marker = u16::from_be_bytes([data[0], data[1]]) as usize;
-        if let Some(description) = marker_mapping.get(&marker) {
-            println!("{}", description);
-        }
-
-        match marker {
-            0xffd8 => {
-                // Start of Image: Skip 2 bytes
-                data = &data[2..];
+        loop {
+            if data.len() < 2 {
+                break;
             }
-            0xffd9 => {
-                // End of Image: Return
-                return;
-            }
-            0xffda => {
-                // Start of Scan: Skip to the last 2 bytes
-                if data.len() > 2 {
-                    data = &data[data.len() - 2..];
-                } else {
+            let marker = Markers::from_u16(u16::from_be_bytes([data[0], data[1]]));
+
+
+
+            match marker {
+                Markers::StartOfImage => {
+                    // Start of Image: Skip 2 bytes
+                    data = &data[2..];
+                }
+                Markers::EndOfImage => {
+                    // End of Image: Return
                     break;
                 }
-            }
-            _ => {
-                if data.len() >= 4 {
-                    let lenchunk = u16::from_be_bytes([data[2], data[3]]) as usize;
-                    let _chunk = &data[4..=lenchunk + 2];
-
-                    if marker == 0xffc4 {
-                        let chunk = &data[2..];
-                        let table = decode_huffman_2(chunk);
-                        println!("{:?}", table);
-                    } else if marker == 0xffdb {
-                        let chunk = &data[2..lenchunk + 2];
-                        let table = QuantizationTable::new(chunk);
-                       println!("QT: {:?}", table);
-                    } else if marker == 0xffc0 {
-                        let chunk = &data[2..lenchunk + 2];
-                        let detail = decode_frame(chunk);
-                        println!("Frame Detail: {:?}", detail);
-                    }
-
-                    if data.len() >= 2 + lenchunk {
-                        data = &data[2 + lenchunk..];
+                Markers::StartOfScan => {
+                    // Start of Scan: Skip to the last 2 bytes
+                    if data.len() > 2 {
+                        // Start of Scan: Skip to the last 2 bytes
+                        let lenchunk = u16::from_be_bytes([data[2], data[3]]) as usize;
+                        println!("Data length: {}, len: {}", data.len(), lenchunk);
+                        for i in 0..lenchunk + 2 {
+                            println!("{:x}", data[i]);
+                        }
+                        raw_data = data[lenchunk + 2..].to_vec();
+                        data = &data[data.len() - 2..];
                     } else {
-                        println!("BREAK");
                         break;
                     }
+                }
+                _ => {
+                    if data.len() >= 4 {
+                        let lenchunk = u16::from_be_bytes([data[2], data[3]]) as usize;
+                        let _chunk = &data[4..=lenchunk + 2];
+
+                        match marker {
+                            Markers::DefineHuffmanTable => {
+                                let second_table = HuffmanHeader::new(_chunk);
+                                htables.push(second_table);
+                            }
+                            Markers::QuantizationTable => {
+                                let chunk = &data[2..lenchunk + 2];
+                                let table = QuantizationTable::new(chunk);
+                                qtables.push(table);
+                            }
+                            Markers::StartOfFrame => {
+                                let chunk = &data[2..lenchunk + 2];
+                                sof = ImageData::new(chunk);
+                            }
+                            _ => {}
+                        }
+                        if data.len() >= 2 + lenchunk {
+                            data = &data[2 + lenchunk..];
+                        } else {
+                            println!("BREAK");
+                            break;
+                        }
 
 
-                } else {
-                    break 
+                    } else {
+                        break 
+                    }
                 }
             }
         }
+        JPEG { qtables , sof, htables, raw_data }
     }
 }
 
@@ -114,27 +156,32 @@ struct ImageData {
     components: Vec<(u8, u8, u8, u8)>,
 }
 
-fn decode_frame(input: &[u8]) -> ImageData {
-    let mut components: Vec<(u8, u8, u8, u8)> = Vec::new();
-    let mut index = 7;
-    let noc = input[index] as u8;
-    index += 1;
-    for _ in 0..noc {
-        let id = input[index];
-        let sampling_factors = input[index + 1];
-        let h = sampling_factors >> 4;
-        let v = sampling_factors & 0x0f;
-        let table_id = input[index + 2];
-        components.push((id, h, v, table_id));
-        index += 3;
-    }
+impl ImageData {
 
-    ImageData {
-        data_precision: input[2],
-        height: u16::from_be_bytes([input[3], input[4]]),
-        width: u16::from_be_bytes([input[5], input[6]]),
-        noc,
-        components,
+    fn empty() -> Self { ImageData { data_precision: 0, height: 0, width: 0, noc: 0, components: vec![] } }
+
+    fn new(input: &[u8]) -> Self {
+        let mut components: Vec<(u8, u8, u8, u8)> = Vec::new();
+        let mut index = 7;
+        let noc = input[index];
+        index += 1;
+        for _ in 0..noc {
+            let id = input[index];
+            let sampling_factors = input[index + 1];
+            let h = sampling_factors >> 4;
+            let v = sampling_factors & 0x0f;
+            let table_id = input[index + 2];
+            components.push((id, h, v, table_id));
+            index += 3;
+        }
+
+        ImageData {
+            data_precision: input[2],
+            height: u16::from_be_bytes([input[3], input[4]]),
+            width: u16::from_be_bytes([input[5], input[6]]),
+            noc,
+            components,
+        }
     }
 }
 
@@ -155,39 +202,29 @@ impl QuantizationTable {
 
 #[derive(Debug)]
 struct HuffmanHeader {
-    length: u16,
-    class_destination: u8,
-    chunk: Vec<u8>,
+    class: u8,
+    destination: u8,
     lengths: Vec<u8>,
     total_symbols: usize,
     symbols: Vec<u8>,
     elements_lengths: Vec<u8>,
 }
 
-fn decode_huffman_2(data: &[u8]) -> HuffmanHeader {
-    let len_chunk = u16::from_be_bytes([data[0], data[1]]) as usize;
-    let class_destination = data[2];
-    let chunk = &data[..len_chunk];
-    let mut data = &data[3..];
+impl HuffmanHeader {
+    fn new(input: &[u8]) -> Self {
+        
+        let cd = input[0];
+        let class = cd >> 4;
+        let destination = cd & 0x0f;
+        let lengths = &input[1..17];
+        let total_symbols: usize = lengths.iter().map(|&x| x as usize).sum();
+        let symbols = &input[17..17 + total_symbols];
 
-    let lengths = &data[..16];
-    data = &data[16..];
-    
-    let total_symbols: usize = lengths.iter().map(|&x| x as usize).sum();
-    let symbols = &data[..total_symbols];
+        let mut elements_lengths = Vec::new();
+        for (length, &count) in lengths.iter().enumerate() {
+            elements_lengths.extend(std::iter::repeat((length + 1) as u8).take(count as usize));
+        }
 
-    let mut elements_lengths = Vec::new();
-    for (length, &count) in lengths.iter().enumerate() {
-        elements_lengths.extend(std::iter::repeat((length + 1) as u8).take(count as usize));
-    }
-
-    HuffmanHeader {
-        length: len_chunk as u16,
-        class_destination,
-        chunk: chunk.to_vec(),
-        lengths: lengths.to_vec(),
-        total_symbols,
-        symbols: symbols.to_vec(),
-        elements_lengths,
+        HuffmanHeader { class, destination, lengths: lengths.to_vec(), total_symbols, symbols: symbols.to_vec(), elements_lengths }
     }
 }
